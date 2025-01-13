f2343292c1f11539eabfab3f02c4f167f583ab33
pacarescuba <94604499+pacarescuba@users.noreply.github.com>
2025-01-13 13:36:51 +0100

    Added solution for Exercise22

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -1,26 +1,40 @@
 package com.campusdual.classroom;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 public class Exercise22 {
 
-    public static List<String> createArrayList() {
+  public static List<String> createArrayList() {
+    List<String> list = new ArrayList<>();
+    list.add("GKFFD");
+    list.add("TNANA");
+    list.add("MPMSL");
+    list.add("PSWME");
+    list.add("LZMLF");
+    list.add("JYEBV");
+    list.add("YELNT");
+    list.add("JSNKR");
+    list.add("JFESF");
+    list.add("TMJLL");
+    return list;
+  }
 
+  public static void printElementsAndIndex(List<String> customList) {
+    int i = 0;
+    for (String s : customList) {
+      System.out.println(i + " " + s);
+      i++;
     }
+  }
 
-    public static void printElementsAndIndex(List<String> customList) {
+  public static boolean addElementToList(List<String> customList, String element) {
+    return customList.add(element);
+  }
 
-    }
-
-    public static boolean addElementToList(List<String> customList, String element) {
-
-    }
-
-    public static void main(String[] args) {
-
-    }
-
-
+  public static void main(String[] args) {
+    printElementsAndIndex(createArrayList());
+  }
 }
